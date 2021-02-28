dd86f24aa2b39bf50947e040507f3059d8572c82
Joel Zisholtz <joelzisholtz@Joels-MBP.attlocal.net>
2021-02-28 02:42:41 -0600

    styled componenets

--- app/javascript/components/Brands/Brands.js
+++ app/javascript/components/Brands/Brands.js
@@ -3,7 +3,30 @@ import axios from "axios";
 import Brand from "./Brand";
 import styled from 'styled-components'
 
-const Home = styled.div``
+const Home = styled.div`
+  text-align: center;
+  max-width: 120px;
+  margin-left: auto;
+  margin-right: auto;
+`
+const Header = styled.div`
+padding: 100px 100px 10px 100px;
+
+h1 {
+  font-size: 42px;
+}
+`
+const Subheader =styled.div`
+font-weight: 300;
+font-size: 26px;
+`
+const Grid = styled.div`
+display: grid;
+grid-template-columns: repeat(4, 1fr);
+grid-gap: 20px;
+width: 100%;
+padding: 20px;
+`
 
 const Brands = () => {
   const [brands, setBrands] = useState([]);
@@ -30,15 +53,15 @@ const Brands = () => {
   });
 
   return (
-    <div className="home">
-      <div className="header">
+    <Home>
+      <Header>
         <h1>Electric Vehicle Brands</h1>
-        <div className="subheader">Electric Vehicle Reviews</div>
-      </div>
-      <div className="grid">
+        <Subheader>Electric Vehicle Reviews</Subheader>
+      </Header>
+      <Grid>
         {grid}
-      </div>
-    </div>
+      </Grid>
+    </Home>
   );
 };
 
