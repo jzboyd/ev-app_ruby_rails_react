3a0ffcc647d49cdbfc5081a76de823a05c92b911
Joel Zisholtz <joelzisholtz@Joels-MBP.attlocal.net>
2021-03-02 14:39:25 -0600

    removed turbolinks

--- app/javascript/components/Brand/Brand.js
+++ app/javascript/components/Brand/Brand.js
@@ -1,6 +1,18 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 
-const Brand = () => {
+const Brand = (props) => {
+    const[brand, setBrand] = useState({})
+    const [review, setReview] = useState({})
+
+    useEffect(() => {
+        const slug = props.match.params/slug
+        const url =`/api/v1/brands/${slug}`
+        
+        axios.get(url)
+        .then( resp => console.log(resp))
+        .catch( resp => console.log(resp))
+    }, [])
     return <div>This is the Brands#show view for our app.</div>
 }
 
