daa5fd3e0015ee345773dab553560d968e923523
Joel Zisholtz <joelzisholtz@Joels-MBP.attlocal.net>
2021-03-05 17:44:51 -0600

    styled components

--- app/javascript/components/Brand/Brand.js
+++ app/javascript/components/Brand/Brand.js
@@ -1,19 +1,65 @@
-import React, {useState, useEffect} from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Header from './Header'
+import styled from 'styled-components'
+
+
+const Wrapper = styled.div`
+    margin-left: auto;
+    margin-right: auto;
+    display: grid;
+    grid-temp;ate-columns: repeat(2, 1fr);
+
+`
+const Column = styled.div`
+background: #fff;
+height: 100vh;
+overflow: scroll;
+
+&: last-child {
+    background: #000;
+}
+`
+const Main = styled.div`
+    padding-left: 50;
+`
+
 
 const Brand = (props) => {
-    const[brand, setBrand] = useState({})
-    const [review, setReview] = useState({})
+  const [brand, setBrand] = useState({});
+  const [review, setReview] = useState({});
+  const[loaded, setLoaded] = useState(false)
 
-    useEffect(() => {
-        const slug = props.match.params.slug
-        const url =`/api/v1/brands/${slug}`
-        
-        axios.get(url)
-        .then( resp => console.log(resp))
-        .catch( resp => console.log(resp))
-    }, [])
-    return <div>This is the Brands#show view for our app.</div>
+  useEffect(() => {
+    const slug = props.match.params.slug;
+    const url = `/api/v1/brands/${slug}`;
+
+    axios
+      .get(url)
+      .then( resp => {
+          setBrand(resp.data)
+          setLoaded(true)
+      })
+      .catch( resp => console.log(resp) )
+  }, [])
+  return (
+    <Wrapper>
+      <Column>
+          <Main>
+        { loaded &&
+          <Header
+            attributes={brand.data.attributes}
+            reviews={brand.included}
+            />
+        }
+            <div className="reviews"></div>
+            </Main>
+            </Column>
+            <Column>
+                <div className="review-form">Review Form Goes Here.</div>
+      </Column>
+    </Wrapper>
+  )
 }
 
-export default Brand
+export default Brand;
---
+++ app/javascript/components/Brand/Header.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import styled from 'styled-componenets'
+
+const Wrapper = styled.div`
+    padding: 50px 100px 50px 0:
+
+    img {
+        height: 50px;
+        width: 50px;
+        border-radius: 100%;
+        border: 1px solid rgba(0,0,0,0.1); 
+    }
+
+    h1{
+        font-size: 30px;
+    }
+`
+const TotalReviews = styled.div`
+    font-size: 18px;
+`
+const TotalOutOf = styled.div``
+
+
+const Header = (props) => {
+    const {name, image_url, avg_score} = props.attributes 
+    const total = props.reviews.length
+
+    return (
+        <div className="wrapper">
+            <h1><img src={image_url} alt={name}/> {name}</h1>
+            <div>
+                <div className="totalReviews">{total} User Reviews</div>
+                <div className="starRating"></div>
+                <div className="totalOutOf">{avg_score} out of 5</div>
+            </div>
+        </div>
+    )
+}
+
+export default Header
